bf97ab095a7b3ecde528279c7cecee56204a7649
Sandyla110 <43601942+Sandyla110@users.noreply.github.com>
2018-11-14 12:16:52 +0100

    Add files via upload

---
+++ Labyrinthe/src/game/Fantome.java
@@ -0,0 +1,72 @@
+package game;
+
+public class Fantome extends Monstres  {
+	
+	public Fantome(Plateau p,int id) {
+		super(p,id);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	protected int nbAleat(int Min,int Max) {
+		return Min + (int)(Math.random() * ((Max - Min) ));
+	}
+	
+	public boolean deplacer() { //se déplace à travers les murs
+		Aleatoire_deplacement_murs();
+		boolean a=Attaquer();
+		return a;
+	
+	}
+	public void Aleatoire_deplacement_murs() {
+
+		int xD=nbAleat(-1,2);
+		int yD=nbAleat(-1,2);// entier aléatoire entre [-1;1]
+		x=x+xD;
+		y=y+yD;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getPoint_de_vie() {
+		return point_de_vie;
+	}
+
+	public void setPoint_de_vie(int point_de_vie) {
+		this.point_de_vie = point_de_vie;
+	}
+
+	public Plateau getP() {
+		return p;
+	}
+
+	public void setP(Plateau p) {
+		this.p = p;
+	}
+	
+	
+	
+
+}
