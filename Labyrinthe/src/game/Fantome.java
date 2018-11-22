e984d68f0e75448e146b2e9e0d620c040b139d55
Sandyla110 <43601942+Sandyla110@users.noreply.github.com>
2018-11-22 15:51:16 +0100

    Add files via upload

--- Labyrinthe/src/game/Fantome.java
+++ Labyrinthe/src/game/Fantome.java
@@ -1,72 +1,88 @@
-package game;
-
 public class Fantome extends Monstres  {
 	
 	public Fantome(Plateau p,int id) {
+		
 		super(p,id);
-		// TODO Auto-generated constructor stub
+// TODO Auto-generated constructor stub
 	}
-
-	
 	protected int nbAleat(int Min,int Max) {
 		return Min + (int)(Math.random() * ((Max - Min) ));
-	}
-	
-	public boolean deplacer() { //se dÃ©place Ã  travers les murs
-		Aleatoire_deplacement_murs();
-		boolean a=Attaquer();
-		return a;
-	
-	}
-	public void Aleatoire_deplacement_murs() {
 
+	}
+	public boolean deplacer() { //se déplace à travers les murs
+
+		Aleatoire_deplacement();
+		traverser_les_murs();
+		boolean a=Attaquer();
+
+		return a;
+
+	}
+
+	public void Aleatoire_deplacement() {
 		int xD=nbAleat(-1,2);
-		int yD=nbAleat(-1,2);// entier alÃ©atoire entre [-1;1]
+		int yD=nbAleat(-1,2);// entier aléatoire entre [-1;1]
 		x=x+xD;
 		y=y+yD;
 	}
-
+	
+	public void traverser_les_murs() {
+		
+		
+		
+		if(p.getPlateau()[x][y]=="X") {
+			//fontome prend la place de mur
+			p.setPlateau(x, y, "F");
+		}
+		else {
+			Aleatoire_deplacement() ;
+			
+		}
+		
+		
+		
+	}
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
 
 	public int getX() {
+
 		return x;
 	}
-
 	public void setX(int x) {
 		this.x = x;
 	}
-
 	public int getY() {
-		return y;
+	return y;
 	}
-
 	public void setY(int y) {
 		this.y = y;
 	}
-
 	public int getPoint_de_vie() {
 		return point_de_vie;
 	}
 
 	public void setPoint_de_vie(int point_de_vie) {
+
 		this.point_de_vie = point_de_vie;
 	}
-
 	public Plateau getP() {
 		return p;
 	}
-
 	public void setP(Plateau p) {
 		this.p = p;
 	}
+
 	
+
 	
+
 	
+
+
 
 }
