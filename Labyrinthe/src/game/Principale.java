1f192a74914479171da69c6b452ef8c23613edb6
ilhamig <43797967+ilhamig@users.noreply.github.com>
2018-11-26 15:23:54 +0100

    Add files via upload

---
+++ Labyrinthe/src/game/Principale.java
@@ -0,0 +1,48 @@
+package game;
+
+import java.util.Scanner;
+import java.awt.Color;
+import java.util.*;
+public class Principale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated metho stub
+		
+		Plateau p=new Plateau(10,10,50);//Création du plateau (nbr ligne,colones,nbr murs obstacle)n
+		//System.out.println(p);
+		System.out.println(p);
+		Fenetre f=new Fenetre(p);//Fenetre d'affichage
+		char Commande=f.getCommande();
+		
+	while (true) {
+		Heros h=p.getHeros();
+		/*Vector<CaseMagique>vm=p.getMagie();
+		Vector<Piege>vp=p.getPiege();
+		System.out.print("Magique :");
+		for(CaseMagique x:vm) {
+			System.out.print (x+ " ");
+		}
+	System.out.println("");
+	System.out.print("Piege :");
+	for(Piege x:vp) {
+		System.out.print (x+ " ");
+	}
+	System.out.println("");*/
+		System.out.println(h);
+
+		f.coeur("coeur.png");
+		f.painMonstre("monstre1.png");
+		f.painMonstre("monstre2.png");
+		f.painMonstre("monstre3.png");
+
+		f.coeur("fond.png");
+		/*
+		f.painHeros2("monstre1.png");
+		f.depainHeros();
+		f.painHeros2("monstre2.png");
+*/
+		f.painMonstre("chemin.png");
+		p.DeplacementMonstre();
+	}
+	}
+	}
